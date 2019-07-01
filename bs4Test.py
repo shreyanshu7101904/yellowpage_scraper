dd2bc28253bacad659ca5b8c0cd476bf25763624
shreyanshu <shreyanshu.shankar@getmyuni.com>
2019-07-01 18:37:46 +0530

    test

--- bs4Test.py
+++ bs4Test.py
@@ -7,17 +7,27 @@ soup = BeautifulSoup(res.text, "html.parser")
 print(soup.title)
 count = 1
 # pdb.set_trace()
-for result in soup.find_all('div',class_ = 'result'):
+for result in soup.findAll(True,class_ ='result'):
+    count += 1
+
     # for x in result.find_all('a'):
     #     print(x.get('href', x.get_text()))
+    data = {}
     url = result.find('a', class_= 'business-name')
     add = result.find('p', class_ = 'adr')
     # print(add.get_text())
     phone = result.find('div', class_ ='phones')
     if phone:
-        print(phone)
-        print(phone.text)
-        print("\n\n",url.get_text(), url.get('href'), url.get('id'))
+        # print(phone)
+        # print(phone.text)
+        data["phone"] =phone.text
+        data["name"] = url.get_text()
+        data["url"] = url.get('href')
+        data["address"] = add
     else:
-
-        print("\n\n",url.get_text(), url.get('href'), url.get('id'))
+        # data["phone"] =phone.text
+        data["name"] = url.get_text()
+        data["url"] = url.get('href')
+        data["address"] = add
+    print(data, "\n\n")
+print(count)
--- test_scrapy.py
+++ test_scrapy.py
@@ -15,28 +15,30 @@ class Crawl(scrapy.Spider):
     def start_requests(self):
         USER_AGENT = "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36"
         Url = 'https://www.yellowpages.com/los-angeles-ca/spa'
-        for x in range(1,5):
-            url = Url + "?page=" + str(x)               
-            yield scrapy.Request(url = url, callback = self.parse, )
+               
+        yield scrapy.Request(url = Url, callback = self.parse, )
 
 
     def parse(self, response):
         output_json = {}
         #pdb.set_trace()
-        data = response.xpath(".//script[@type = 'application/ld+json'][2]").get()
+        count = 1
+        # data = response.xpath(".//script[@type = 'application/ld+json'][2]").get()
         for sub_list in response.xpath(".//div[@class='result']/div/div[@class= 'v-card']"):
-            time.sleep(0.5)
+            # time.sleep(0.5)
             output_json["Name"] = sub_list.xpath(".//h2/a/span/text()").get()
             output_json["Image_Url"] = sub_list.xpath(".//div[@class='media-thumbnail']//img/@src").get()
             yp_url = sub_list.xpath('.//h2/a/@href').get()
             output_json["Url"] = yp_url            
-            #print(sub_list.xpath(".//div[contains(@class, 'info-secondary')]/div[contains(@class, 'phone')]/text()").get())            
+            print(sub_list.xpath(".//div[contains(@class, 'info-secondary')]/div[contains(@class, 'phone')]/text()").get())            
             output_json["Phone"] = sub_list.xpath(".//div[contains(@class, 'info-secondary')]/div[contains(@class, 'phone')]/text()").get()
             output_json["Address"] = sub_list.xpath(".//div[contains(@class, 'info-secondary')]/div[contains(@class, 'street-address')]/text()").get()
             output_json["Locality"] = sub_list.xpath(".//div[contains(@class, 'info-secondary')]/div[contains(@class,'locality')]/text()").get()
             for links in sub_list.xpath(".//div[contains(@class, 'links')]/a"):
                 output_json[links.xpath(".//text()").get()] = links.xpath(".//@href").get()              
             # print(output_json)
-        print(data)
+            count += 1
+            print("count", count)
+            print(output_json)
         print("///////////////////////////")
             #output_json = {}
