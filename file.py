1b72d2d11ce17a742768d3a67e3923044652f98f
shreyanshu <shreyanshu.shankar@getmyuni.com>
2019-07-02 10:16:52 +0530

    test

---
+++ file.py
@@ -0,0 +1,91 @@
+None
+count 2
+{'Name': 'Spa', 'Image_Url': None, 'Url': '/los-angeles-ca/mip/spa-542050935', 'Phone': None, 'Address': None, 'Locality': None}
+None
+count 3
+{'Name': 'Spa', 'Image_Url': None, 'Url': '/los-angeles-ca/mip/spa-547372883', 'Phone': None, 'Address': None, 'Locality': None}
+None
+count 4
+{'Name': 'Spa', 'Image_Url': None, 'Url': '/los-angeles-ca/mip/spa-544528763', 'Phone': None, 'Address': None, 'Locality': None}
+None
+count 5
+{'Name': 'Spa', 'Image_Url': None, 'Url': '/los-angeles-ca/mip/spa-548691747', 'Phone': None, 'Address': None, 'Locality': None}
+None
+count 6
+{'Name': 'Spa', 'Image_Url': None, 'Url': '/los-angeles-ca/mip/spa-30873500', 'Phone': None, 'Address': None, 'Locality': None}
+None
+count 7
+{'Name': 'Spa', 'Image_Url': None, 'Url': '/los-angeles-ca/mip/spa-530118651', 'Phone': None, 'Address': None, 'Locality': None}
+None
+count 8
+{'Name': 'JFS Pool & Spa Service', 'Image_Url': 'https://i2.ypcdn.com/blob/cfc1bb96fa1733995c1a6d713c834034a0810ea6_50x50_crop.jpg?4683bd1\n', 'Url': '/los-angeles-ca/mip/jfs-pool-spa-service-481517689?lid=1002004361311', 'Phone': None, 'Address': None, 'Locality': None, 'Website': 'http://jfspoolandspa.com', 'Directions': '/listings/1002004361311/directions', 'More Info': '/los-angeles-ca/mip/jfs-pool-spa-service-481517689?lid=1002004361311'}
+None
+count 9
+{'Name': "Levon's Hair Boutique", 'Image_Url': 'https://i2.ypcdn.com/blob/9f0eb859fae3612630cfbf3cd275756aa94fed64_50x50_crop.jpg?4683bd1\n', 'Url': '/los-angeles-ca/mip/levons-hair-boutique-9269615?lid=1001092619149', 'Phone': None, 'Address': None, 'Locality': None, 'Website': 'http://jfspoolandspa.com', 'Directions': '/listings/1001092619149/directions', 'More Info': '/los-angeles-ca/mip/jfs-pool-spa-service-481517689?lid=1002004361311', 'Services': '/listings/9269615/menu?lid=1001092619149'}
+None
+count 10
+{'Name': 'Faces European Skin Care', 'Image_Url': 'https://i2.ypcdn.com/blob/f0fc09001e888b85bec05f93c379a4c59ba1972a_50x50_crop.jpg?4683bd1\n', 'Url': '/los-angeles-ca/mip/faces-european-skin-care-22251394', 'Phone': None, 'Address': None, 'Locality': None, 'Website': 'http://www.facesesc.com', 'Directions': '/listings/1001092619149/directions', 'More Info': '/los-angeles-ca/mip/jfs-pool-spa-service-481517689?lid=1002004361311', 'Services': '/listings/22251394/menu?lid=22251394'}
+None
+count 11
+{'Name': 'Olympic Spa', 'Image_Url': None, 'Url': '/los-angeles-ca/mip/olympic-spa-5650818', 'Phone': None, 'Address': None, 'Locality': None, 'Website': 'http://www.olympicspala.com', 'Directions': '/listings/1001092619149/directions', 'More Info': '/los-angeles-ca/mip/jfs-pool-spa-service-481517689?lid=1002004361311', 'Services': '/listings/5650818/menu?lid=5650818'}
+None
+count 12
+{'Name': 'Bliss Spa Los Angeles- Westwood', 'Image_Url': None, 'Url': '/los-angeles-ca/mip/bliss-spa-los-angeles-westwood-12208622', 'Phone': None, 'Address': None, 'Locality': None, 'Website': 'http://www.blissworld.com', 'Directions': '/listings/1001092619149/directions', 'More Info': '/los-angeles-ca/mip/jfs-pool-spa-service-481517689?lid=1002004361311', 'Services': '/listings/12208622/menu?lid=12208622'}
+None
+count 13
+{'Name': 'Kalaya Spa', 'Image_Url': None, 'Url': '/los-angeles-ca/mip/kalaya-spa-7377577', 'Phone': None, 'Address': None, 'Locality': None, 'Website': 'http://www.blissworld.com', 'Directions': '/listings/1001092619149/directions', 'More Info': '/los-angeles-ca/mip/jfs-pool-spa-service-481517689?lid=1002004361311', 'Services': '/listings/7377577/menu?lid=7377577'}
+None
+count 14
+{'Name': 'Pure Cleansing Center', 'Image_Url': None, 'Url': '/los-angeles-ca/mip/pure-cleansing-center-467152500', 'Phone': None, 'Address': None, 'Locality': None, 'Website': 'http://www.purecenter.com', 'Directions': '/listings/1001092619149/directions', 'More Info': '/los-angeles-ca/mip/jfs-pool-spa-service-481517689?lid=1002004361311', 'Services': '/listings/467152500/menu?lid=467152500'}
+None
+count 15
+{'Name': 'Tranquility Body Spa / Massage in Culver City', 'Image_Url': 'https://i2.ypcdn.com/blob/bbab41360205e1bc8117e9a1fa2fb8410bcf1b96_50x50_crop.jpg?4683bd1\n', 'Url': '/los-angeles-ca/mip/tranquility-body-spa-massage-in-culver-city-10286277', 'Phone': None, 'Address': None, 'Locality': None, 'Website': 'http://www.tranquilitybodyspa.com', 'Directions': '/listings/1001092619149/directions', 'More Info': '/los-angeles-ca/mip/jfs-pool-spa-service-481517689?lid=1002004361311', 'Services': '/listings/10286277/menu?lid=10286277'}
+None
+count 16
+{'Name': 'Awe Spa', 'Image_Url': 'https://i2.ypcdn.com/blob/d4b0670ea6745a6fbe7b532a3dfa491db610da45_50x50_crop.jpg?4683bd1\n', 'Url': '/los-angeles-ca/mip/awe-spa-459711339', 'Phone': None, 'Address': None, 'Locality': None, 'Website': 'http://www.awe-spa.com', 'Directions': '/listings/1001092619149/directions', 'More Info': '/los-angeles-ca/mip/jfs-pool-spa-service-481517689?lid=1002004361311', 'Services': '/listings/459711339/menu?lid=459711339'}
+None
+count 17
+{'Name': 'Spa Luce', 'Image_Url': None, 'Url': '/los-angeles-ca/mip/spa-luce-1334481', 'Phone': None, 'Address': None, 'Locality': None, 'Website': 'http://www.awe-spa.com', 'Directions': '/listings/1001092619149/directions', 'More Info': '/los-angeles-ca/mip/jfs-pool-spa-service-481517689?lid=1002004361311', 'Services': '/listings/1334481/menu?lid=1334481'}
+None
+count 18
+{'Name': 'Nightheron Spa', 'Image_Url': 'https://i2.ypcdn.com/blob/e6314ade65f1db5a529f32c24d6d3d5408bd3b11_50x50_crop.jpg?4683bd1\n', 'Url': '/los-angeles-ca/mip/nightheron-spa-475655145', 'Phone': None, 'Address': None, 'Locality': None, 'Website': 'http://www.awe-spa.com', 'Directions': '/listings/1001092619149/directions', 'More Info': '/los-angeles-ca/mip/jfs-pool-spa-service-481517689?lid=1002004361311', 'Services': '/listings/1334481/menu?lid=1334481'}
+None
+count 19
+{'Name': 'Bailan Body Works', 'Image_Url': None, 'Url': '/los-angeles-ca/mip/bailan-body-works-465214757', 'Phone': None, 'Address': None, 'Locality': None, 'Website': 'http://www.awe-spa.com', 'Directions': '/listings/1001092619149/directions', 'More Info': '/los-angeles-ca/mip/jfs-pool-spa-service-481517689?lid=1002004361311', 'Services': '/listings/1334481/menu?lid=1334481'}
+None
+count 20
+{'Name': 'Palazzo At Park Labrea', 'Image_Url': None, 'Url': '/los-angeles-ca/mip/palazzo-at-park-labrea-467504421', 'Phone': None, 'Address': None, 'Locality': None, 'Website': 'http://www.palazzo.com', 'Directions': '/listings/1001092619149/directions', 'More Info': '/los-angeles-ca/mip/jfs-pool-spa-service-481517689?lid=1002004361311', 'Services': '/listings/1334481/menu?lid=1334481'}
+None
+count 21
+{'Name': 'Carasoin Spa', 'Image_Url': None, 'Url': '/los-angeles-ca/mip/carasoin-spa-451040157', 'Phone': None, 'Address': None, 'Locality': None, 'Website': 'http://www.carasoin.com', 'Directions': '/listings/1001092619149/directions', 'More Info': '/los-angeles-ca/mip/jfs-pool-spa-service-481517689?lid=1002004361311', 'Services': '/listings/451040157/menu?lid=451040157'}
+None
+count 22
+{'Name': 'Vortex Advanced Cosmetic & Spa', 'Image_Url': None, 'Url': '/los-angeles-ca/mip/vortex-advanced-cosmetic-spa-475536208', 'Phone': None, 'Address': None, 'Locality': None, 'Website': 'http://www.vortexspa.net', 'Directions': '/listings/1001092619149/directions', 'More Info': '/los-angeles-ca/mip/jfs-pool-spa-service-481517689?lid=1002004361311', 'Services': '/listings/451040157/menu?lid=451040157'}
+None
+count 23
+{'Name': 'Beverly Hills Chakra Cleansing Spa', 'Image_Url': None, 'Url': '/los-angeles-ca/mip/beverly-hills-chakra-cleansing-spa-465264162', 'Phone': None, 'Address': None, 'Locality': None, 'Website': 'http://www.beverlyhillschakraspa.com', 'Directions': '/listings/1001092619149/directions', 'More Info': '/los-angeles-ca/mip/jfs-pool-spa-service-481517689?lid=1002004361311', 'Services': '/listings/451040157/menu?lid=451040157'}
+None
+count 24
+{'Name': 'Lizzy Wax', 'Image_Url': None, 'Url': '/los-angeles-ca/mip/lizzy-wax-473592832', 'Phone': None, 'Address': None, 'Locality': None, 'Website': 'http://www.beverlyhillschakraspa.com', 'Directions': '/listings/1001092619149/directions', 'More Info': '/los-angeles-ca/mip/jfs-pool-spa-service-481517689?lid=1002004361311', 'Services': '/listings/451040157/menu?lid=451040157'}
+None
+count 25
+{'Name': 'Beba Beauty', 'Image_Url': 'https://i2.ypcdn.com/blob/d3e827c71e4d0eca66ceccc6297af59046787823_50x50_crop.jpg?4683bd1\n', 'Url': '/los-angeles-ca/mip/beba-beauty-5446411', 'Phone': None, 'Address': None, 'Locality': None, 'Website': 'http://www.bebabeauty.com', 'Directions': '/listings/1001092619149/directions', 'More Info': '/los-angeles-ca/mip/jfs-pool-spa-service-481517689?lid=1002004361311', 'Services': '/listings/5446411/menu?lid=5446411'}
+None
+count 26
+{'Name': 'Hugh Spa', 'Image_Url': 'https://i2.ypcdn.com/blob/065aa1ae3b84994ac88778add6e3ee719fbd0901_50x50_crop.jpg?4683bd1\n', 'Url': '/los-angeles-ca/mip/hugh-spa-464579735', 'Phone': None, 'Address': None, 'Locality': None, 'Website': 'http://www.hughspa.com', 'Directions': '/listings/1001092619149/directions', 'More Info': '/los-angeles-ca/mip/jfs-pool-spa-service-481517689?lid=1002004361311', 'Services': '/listings/464579735/menu?lid=464579735'}
+None
+count 27
+{'Name': 'Forty Winks Total Relaxation', 'Image_Url': 'https://i2.ypcdn.com/blob/74b0e9205c417049635b0c24e7b069c6c80f8498_50x50_crop.jpg?4683bd1\n', 'Url': '/los-angeles-ca/mip/forty-winks-total-relaxation-478384934', 'Phone': None, 'Address': None, 'Locality': None, 'Website': 'http://www.hughspa.com', 'Directions': '/listings/1001092619149/directions', 'More Info': '/los-angeles-ca/mip/jfs-pool-spa-service-481517689?lid=1002004361311', 'Services': '/listings/478384934/menu?lid=478384934'}
+None
+count 28
+{'Name': 'Visage de Layla', 'Image_Url': None, 'Url': '/los-angeles-ca/mip/visage-de-layla-468873210', 'Phone': None, 'Address': None, 'Locality': None, 'Website': 'http://www.visagedelayla.com', 'Directions': '/listings/1001092619149/directions', 'More Info': '/los-angeles-ca/mip/jfs-pool-spa-service-481517689?lid=1002004361311', 'Services': '/listings/478384934/menu?lid=478384934'}
+None
+count 29
+{'Name': 'Daing Ki Beauty Salon & Sauna', 'Image_Url': 'https://i2.ypcdn.com/blob/ef82575b93bfd53431ff240f009e804875d468aa_50x50_crop.jpg?4683bd1\n', 'Url': '/los-angeles-ca/mip/daing-ki-beauty-salon-sauna-12425442', 'Phone': None, 'Address': None, 'Locality': None, 'Website': 'http://daengkispa.com', 'Directions': '/listings/1001092619149/directions', 'More Info': '/los-angeles-ca/mip/jfs-pool-spa-service-481517689?lid=1002004361311', 'Services': '/listings/12425442/menu?lid=12425442'}
+None
+count 30
+{'Name': 'Aqua Thai Spa', 'Image_Url': None, 'Url': '/los-angeles-ca/mip/aqua-thai-spa-468216553', 'Phone': None, 'Address': None, 'Locality': None, 'Website': 'http://www.aquathaispa.com', 'Directions': '/listings/1001092619149/directions', 'More Info': '/los-angeles-ca/mip/jfs-pool-spa-service-481517689?lid=1002004361311', 'Services': '/listings/468216553/menu?lid=468216553'}
+None
+count 31
+{'Name': 'Grand Spa', 'Image_Url': None, 'Url': '/los-angeles-ca/mip/grand-spa-15520624', 'Phone': None, 'Address': None, 'Locality': None, 'Website': 'http://www.grandspala.com', 'Directions': '/listings/1001092619149/directions', 'More Info': '/los-angeles-ca/mip/jfs-pool-spa-service-481517689?lid=1002004361311', 'Services': '/listings/15520624/menu?lid=15520624'}
+///////////////////////////
