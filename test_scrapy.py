636ce1b29c30bff683e284d74219699fd72e54b5
shreyanshu <shreyanshu.shankar@getmyuni.com>
2019-07-02 11:01:44 +0530

    updated script

--- test_scrapy.py
+++ test_scrapy.py
@@ -12,7 +12,7 @@ class Crawl(scrapy.Spider):
 	}
 
     def start_requests(self):
-        USER_AGENT = "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36"
+        user_agent = "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
         Url = 'https://www.yellowpages.com/los-angeles-ca/spa'
                
         yield scrapy.Request(url = Url, callback = self.parse, )
@@ -23,16 +23,16 @@ class Crawl(scrapy.Spider):
         #pdb.set_trace()
         count = 1
         # data = response.xpath(".//script[@type = 'application/ld+json'][2]").get()
-        for sub_list in response.xpath(".//div[@class='result']/div/div[@class= 'v-card']"):
+        for sub_list in response.xpath(".//div[contains(@class,'search-result')]/div[@class='result']/div/div[@class= 'v-card']"):
             # time.sleep(0.5)
             output_json["Name"] = sub_list.xpath(".//h2/a/span/text()").get()
-            output_json["Image_Url"] = sub_list.xpath(".//div[@class='media-thumbnail']//img/@src").get()
+            output_json["Image_Url"] = sub_list.xpath(".//div[@class='media-thumbnail']/a[contains(@class, 'media-thumbnail')]/img/@src").get()
             yp_url = sub_list.xpath('.//h2/a/@href').get()
             output_json["Url"] = yp_url            
-            print(sub_list.xpath(".//div[contains(@class, 'info-secondary')]/div[contains(@class, 'phone')]/text()").get())            
+            #print(sub_list.xpath(".//div[contains(@class, 'info-secondary')]/div[contains(@class, 'phone')]/text()").get())            
             output_json["Phone"] = sub_list.xpath(".//div[contains(@class, 'phone')]/text()").get()
-            output_json["Address"] = sub_list.xpath(".//div[contains(@class, 'adr')]/div[@class= 'street-address']/text()").get()
-            output_json["Locality"] = sub_list.xpath(".//div[contains(@class,'adr')]/div[@class= 'locality']/text()").get()
+            output_json["Address"] = sub_list.xpath(".//p[@class= 'adr']/span[@class= 'street-address']/text()").get()
+            output_json["Locality"] = sub_list.xpath(".//p[@class='adr']/span[@class= 'locality']/text()").get()
             for links in sub_list.xpath(".//div[contains(@class, 'links')]/a"):
                 output_json[links.xpath(".//text()").get()] = links.xpath(".//@href").get()              
             # print(output_json)
