df90d93c95742ec97e04c1a66d8781cc96ad5a92
shreyanshu <shreyanshu.shankar@getmyuni.com>
2019-07-02 11:14:09 +0530

    removed comments

--- test_scrapy.py
+++ test_scrapy.py
@@ -1,6 +1,4 @@
 import scrapy
-from scrapy.selector import Selector
-
 import time 
 
 class Crawl(scrapy.Spider):
@@ -20,24 +18,16 @@ class Crawl(scrapy.Spider):
 
     def parse(self, response):
         output_json = {}
-        #pdb.set_trace()
-        count = 1
-        # data = response.xpath(".//script[@type = 'application/ld+json'][2]").get()
         for sub_list in response.xpath(".//div[contains(@class,'search-result')]/div[@class='result']/div/div[@class= 'v-card']"):
-            # time.sleep(0.5)
             output_json["Name"] = sub_list.xpath(".//h2/a/span/text()").get()
             output_json["Image_Url"] = sub_list.xpath(".//div[@class='media-thumbnail']/a[contains(@class, 'media-thumbnail')]/img/@src").get()
             yp_url = sub_list.xpath('.//h2/a/@href').get()
-            output_json["Url"] = yp_url            
-            #print(sub_list.xpath(".//div[contains(@class, 'info-secondary')]/div[contains(@class, 'phone')]/text()").get())            
+            output_json["Url"] = yp_url                    
             output_json["Phone"] = sub_list.xpath(".//div[contains(@class, 'phone')]/text()").get()
             output_json["Address"] = sub_list.xpath(".//p[@class= 'adr']/span[@class= 'street-address']/text()").get()
             output_json["Locality"] = sub_list.xpath(".//p[@class='adr']/span[@class= 'locality']/text()").get()
             for links in sub_list.xpath(".//div[contains(@class, 'links')]/a"):
                 output_json[links.xpath(".//text()").get()] = links.xpath(".//@href").get()              
-            # print(output_json)
-            #count += 1
-            #print("count", count)
             print(output_json)
         print("///////////////////////////")
-            #output_json = {}
+
