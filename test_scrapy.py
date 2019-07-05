16ca69eec47d3d3a62c4debd1333bfe88b73808e
shreyanshu7101904 <shreyanshu7101904@gmail.com>
2019-07-05 16:10:21 +0530

    functional script

--- test_scrapy.py
+++ test_scrapy.py
@@ -1,7 +1,9 @@
 import scrapy
-import time 
-import pdb
-from mongodbfunc import putDataInDb
+import datetime
+import re
+from pymongo import MongoClient
+
+
 class Crawl(scrapy.Spider):
     """ scrapper yellow pages list"""
     name = "Test"
@@ -10,6 +12,13 @@ class Crawl(scrapy.Spider):
 		"DOWNLOAD_DELAY" : 0.50,
 	}
 
+    def putDataInDb(self, doc, value):
+        ob = MongoClient()
+        value["Date"] = str(datetime.date.today())
+        db = ob.yellowpages_info[doc]
+        ids = db.insert_one(value).inserted_id
+        print(ids)
+
     def generateLinks(self):
         links = []
         business = ["plumbers","dentist", "attorneys"]
@@ -17,7 +26,10 @@ class Crawl(scrapy.Spider):
         for b, l in zip(business, location):
             links.append("https://www.yellowpages.com"+"/"+l + "/" + b)
         return links
-    
+
+        def key_filter(self, key):
+            s1 = re.sub('[^A-Za-z0-9]+', '_',key.lower().strip())
+            return s1
 
     def start_requests(self):
         user_agent = "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
@@ -26,39 +38,29 @@ class Crawl(scrapy.Spider):
         for link in links:
             for number in range(1,10):
                 url = link + "?page=" + str(number)             
-                yield scrapy.Request(url = url, callback = self.parse,meta= {"request_url":url} )
+        yield scrapy.Request(url = "https://www.yellowpages.com/los-angeles-ca/plumbers?page=1", callback = self.parse,meta= {"request_url":url} )
 
 
     def parse(self, response):
-        # print(response.text)
         for cards in response.xpath(".//div[@class = 'scrollable-pane']/div[contains(@class,'organic')]/div[@class='result']"):
             output_json = {}
             output_json["request_url"] = response.meta["request_url"]
             output_json["response_url"] = str(response.url)
-            # srp-listing clickable-area paid-name sp
             sub_list =cards.xpath(".//div/div[@class= 'v-card']")
-            # print(sub_list)
-            output_json["Name"] = sub_list.xpath(".//h2/a/span/text()").get()
-            
+            output_json["Name"] = sub_list.xpath(".//h2/a/span/text()").get()         
             ids = cards.xpath(".//@id").get()
             output_json["Image_Url"] = sub_list.xpath(".//div[@class='media-thumbnail']/a[contains(@class, 'media-thumbnail')]/img/@src").get()
             if ids:
                 output_json["id"] = ids.split("-")[1]
             yp_url = sub_list.xpath('.//h2/a/@href').get()
-            output_json["Url"] = yp_url                    
-            output_json["Phone"] = sub_list.xpath(".//div[contains(@class, 'phone')]/text()").get()
-            output_json["Address"] = sub_list.xpath(".//div[contains(@class, 'info-primary')]/p[@class= 'adr']/span[@class= 'street-address']/text()").get()
+            output_json["url_yellowpage"] = yp_url                    
+            output_json["phone"] = sub_list.xpath(".//div[contains(@class, 'phone')]/text()").get()
+            output_json["address"] = sub_list.xpath(".//div[contains(@class, 'info-primary')]/p[@class= 'adr']/span[@class= 'street-address']/text()").get()
             
-            output_json["Locality"] = sub_list.xpath(".//div[contains(@class, 'info-primary')]/p[@class='adr']/span[@class= 'locality']/text()").get()
-            output_json["Region"] = sub_list.xpath(".//div[contains(@class, 'info-primary')]/p[@class= 'adr']/span[3]/text()").get()
-            output_json["ZipCode"] = sub_list.xpath(".//div[contains(@class, 'info-primary')]/p[@class= 'adr']/span[4]/text()").get()
-            print(output_json["Name"],sub_list.xpath(".//div[contains(@class, 'info-primary')]/p[@class= 'adr']/span[@class= 'street-address']").get())
-            # output_json["Zipcode"] = sub_list.xpath(".//p[@class='adr']/span[last()]/text()").get()
+            output_json["locality"] = sub_list.xpath(".//div[contains(@class, 'info-primary')]/p[@class='adr']/span[@class= 'locality']/text()").get()
+            output_json["region"] = sub_list.xpath(".//div[contains(@class, 'info-primary')]/p[@class= 'adr']/span[3]/text()").get()
+            output_json["zipcode"] = sub_list.xpath(".//div[contains(@class, 'info-primary')]/p[@class= 'adr']/span[4]/text()").get()
             for links in sub_list.xpath(".//div[contains(@class, 'links')]/a"):
-                output_json[links.xpath(".//text()").get()] = links.xpath(".//@href").get()              
-            putDataInDb("yellowpages_data", output_json)
-            #print(output_json)
-            # print(count)
-            # print(output_json["Name"])
-            # print(output_json["Address"])
-            # print("******", output_json)
+
+                output_json[links.xpath(".//text()").get()] = links.xpath(".//@href").get()   
+            self.putDataInDb("yellowpages_data", output_json)
